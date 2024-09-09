fda2a10082bc09f6958a9d16367173c37ff39431
Pedro Pereira <pepereira@reseautique.osq>
2024-09-09 12:02:07 -0400

    Monday from School

--- Exercises/Assets/DoorOpening.cs
+++ Exercises/Assets/DoorOpening.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
 {
-    
+    [SerializeField] private float _speed = 1f;
+    private float _timer;
+    private float _negativeOrPositive = 1;
+
+    void Update()
+    {
+        transform.localScale += new Vector3(0,_negativeOrPositive * _speed,0) * Time.deltaTime;
+        if (transform.localScale.y > 1)
+        {
+            _negativeOrPositive = -1;
+        }
+    }
 }
+
+
