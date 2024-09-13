0733dc697e648cd2521ff921fb9d874ef27df983
Pedro Pereira <pepereira@reseautique.osq>
2024-09-13 11:59:45 -0400

    Fridat from School

--- Exercises/Assets/BallsManager.cs
+++ Exercises/Assets/BallsManager.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class BallsManager : MonoBehaviour
 {
-    [SerializeField] private MoveBalls blackballMoveScript;
-    [SerializeField] private MoveBalls whiteballMoveScript;
+    [SerializeField] private MoveBalls _blackballMoveScript;
+    [SerializeField] private MoveBalls _whiteballMoveScript;
+    [SerializeField] private Collider _blackwall;
+    [SerializeField] private Collider _whitewall;
 
-   
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if (blackballMoveScript != null) blackballMoveScript.enabled = true;
-            if (whiteballMoveScript != null) whiteballMoveScript.enabled = false;
+            if (_blackballMoveScript != null) _blackballMoveScript.enabled = true;
+            if (_whiteballMoveScript != null) _whiteballMoveScript.enabled = false;
+
+            if (_blackwall != null) _blackwall.enabled = false;
+            if (_whitewall != null) _whitewall.enabled = true;
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (blackballMoveScript != null) blackballMoveScript.enabled = false;
-            if (whiteballMoveScript != null) whiteballMoveScript.enabled = true;
+            if (_blackballMoveScript != null) _blackballMoveScript.enabled = false;
+            if (_whiteballMoveScript != null) _whiteballMoveScript.enabled = true;
+
+            if (_whitewall != null) _whitewall.enabled = false;
+            if (_blackwall != null) _blackwall.enabled = true;
+            
         }
     }
 }
