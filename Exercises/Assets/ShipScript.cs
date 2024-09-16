305bcc9aecd5815e30135c6a980bba6eb677219c
Pedro Pereira <a.pedropereira@hotmail.com>
2024-09-15 23:21:36 -0400

    Sunday from home night

--- Exercises/Assets/ShipScript.cs
+++ Exercises/Assets/ShipScript.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ShipScript : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _rotation;
     [SerializeField] Rigidbody2D _rigidbody;
+    [SerializeField] private float _acceleration = 0.5f;
+    [SerializeField] private float _deceleration = 1f;
+    [SerializeField] private float _maxSpeed = 7f;
+    [SerializeField] private float _currentSpeed = 0f;
 
     private void Start()
     {
@@ -29,12 +34,21 @@ public class ShipScript : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            _rigidbody.velocity = transform.up * _speed;
+            _currentSpeed += _acceleration * Time.deltaTime; 
+            _currentSpeed = Mathf.Clamp(_currentSpeed, 0, _maxSpeed);
         }
 
         else if (Input.GetKey(KeyCode.S))
         {
-            _rigidbody.velocity = Vector2.zero;
+            _currentSpeed -= _deceleration * Time.deltaTime; 
+            _currentSpeed = Mathf.Clamp(_currentSpeed, 0, _maxSpeed);
         }
+
+        else 
+        {
+            _currentSpeed = Mathf.Lerp(_currentSpeed, 0, _deceleration * Time.deltaTime);
+        }
+
+        _rigidbody.velocity = transform.up * _currentSpeed;
     }
 }
